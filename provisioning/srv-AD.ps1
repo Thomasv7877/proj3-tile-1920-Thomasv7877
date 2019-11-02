8c8c1cf5fb484ee78e31a8e8ac4f92a8ec8c223f
Thomas Van den Hauwe <thomas.vandenhauwe.v7877@student.hogent.be>
2019-11-02 16:56:36 +0100

    scripting AD server

--- provisioning/srv-AD.ps1
+++ provisioning/srv-AD.ps1
@@ -39,16 +39,18 @@ function config_adds {
     -SysvolPath "C:\Windows\SYSVOL" `
     -Force:$true
 }
+
+# REBOOT
     
 function dns_put_ip{
     Set-DnsClientServerAddress -InterfaceAlias $hostonly_name -ServerAddresses ($hostonly_ip)
 }
     
 function dns_extra_zones{ # todo: de reverse zone?
-    
+    Add-DnsServerPrimaryZone -NetworkId "192.168.56.0/24" -ReplicationScope Forest
 }
     
-function config_nat{
+function config_nat{ # WERKT NIET !!!
 
     # legacy remote access menu enabelen
     #Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Services\RemoteAccess\Parameters' -Name 'ModernStackEnabled' -value 0
@@ -57,18 +59,18 @@ function config_nat{
     Install-RemoteAccess -VpnType Vpn
      
     cmd.exe /c "netsh routing ip nat install"
-    Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Services\RemoteAccess\Parameters' -Name 'ModernStackEnabled' -value 0
-    # onderstaande is overbodig
+    
+    # onderstaande is overbodig?
     cmd.exe /c "netsh routing ip nat add interface $nat_name"
     cmd.exe /c "netsh routing ip nat set interface $hostonly_name mode=full"
     cmd.exe /c "netsh routing ip nat add interface $hostonly_name"
-
+    Set-Itemproperty -path 'HKLM:\SYSTEM\ControlSet001\Services\RemoteAccess\Parameters' -Name 'ModernStackEnabled' -value 0
 }
     
 function config_dhcp{
 
     $User = "localhost\Administrator"
-    $PWord = (ConvertTo-SecureString -Strindns_pu   g "vagrant" -AsPlainText -Force)
+    $PWord = (ConvertTo-SecureString -String "vagrant" -AsPlainText -Force)
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
     $cs = New-CimSession -Credential $credential -ComputerName localhost
 
@@ -77,28 +79,6 @@ function config_dhcp{
     Get-DhcpServerInDC # authorizatie checken
     Add-DhcpServerv4Scope -name $scope_name -StartRange $start -EndRange $end -SubnetMask $mask -State Active
     # Add-DhcpServerv4ExclusionRange -ScopeID 10.0.0.0 -StartRange 10.0.0.1 -EndRange 10.0.0.15
-}
-
-function create_config_container {
-
-    $DomainDN = (Get-ADDomain).DistinguishedName
-    $ThisSiteSystem = Get-ADComputer $env:ComputerName 
-    $SystemDN = "CN=System," + $DomainDN
-    $Container = New-ADObject -Type Container -name "System Management" -Path "$SystemDN" -Passthru
- 
-    $ACL = Get-ACL -Path AD:\$Container
-
-    $SID = [System.Security.Principal.SecurityIdentifier] $ThisSiteSystem.SID
-
-    $adRights = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
-    $type = [System.Security.AccessControl.AccessControlType] "Allow"
-    $inheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
-    $ACE = New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
-                                     $SID,$adRights,$type,$inheritanceType
-
-    $ACL.AddAccessRule($ACE)
-
-    Set-ACL -AclObject $ACL -Path "AD:$Container"
 }
 
 function create_config_container { # effect??
@@ -130,6 +110,16 @@ function extend_ad_schema {
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
     Start-Process -FilePath C:\Sources\SC_Configmgr_SCEP_1902\SMSSETUP\BIN\X64\extadsch.exe -Wait -Credential $credential
+}
+
+function config_firewall {
+    $gpo1 = "Client Push Policy Settings"
+    $gpo2 = "SQL Ports for SCCM"
+
+    new-gpo -name $gpo1
+    import-GPO -Domain $dom_name -BackupId 6EFCAB8F-75E9-48A6-8EE5-FFF481566812 -Path C:\vagrant\provisioning -TargetName $gpo1
+    new-gpo -name $gpo2
+    import-GPO -Domain $dom_name -BackupId 0BA78A36-741F-4573-AAE4-1B2930AA3292 -Path C:\vagrant\provisioning -TargetName $gpo2
 }
 
 # operations:
